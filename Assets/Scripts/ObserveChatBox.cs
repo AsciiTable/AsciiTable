170364e4184109678349beb8aacf1aeb59bed421
Jessica Wei <jessicawei9@gmail.com>
2020-04-17 03:46:41 -0700

    getting photon init

---
+++ Assets/Scripts/ObserveChatBox.cs
@@ -0,0 +1,16 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObserveChatBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.SendRate = 20;
+		PhotonNetwork.sendRateOnSerialize = 10;
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) { 
+	}
+}
